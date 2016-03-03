86671ce1239d66e6ba6e029bdcd8f4bcd9aad211
unknown <alyssonlime@hotmail.com>
2016-03-03 16:56:51 -0300

    Edicao

---
+++ Sistema/u_DF.pas
@@ -0,0 +1,24 @@
+unit u_DF;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  T_DF = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  _DF: T_DF;
+
+implementation
+
+{$R *.dfm}
+
+end.
